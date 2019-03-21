ecc8e78d025d561499b876a4c4e1d45d0110e662
YETA666 <429721783@qq.com>
2019-03-21 13:18:50 +0800

    测试配置中心配置8

--- api-gateway-dynamic-route/src/main/java/com/yeta/apigatewaydynamicroute/ApiGatewayDynamicRouteApplication.java
+++ api-gateway-dynamic-route/src/main/java/com/yeta/apigatewaydynamicroute/ApiGatewayDynamicRouteApplication.java
@@ -1,11 +1,13 @@
 package com.yeta.apigatewaydynamicroute;
 
+import com.yeta.apigatewaydynamicroute.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy        ////开启Zuul的API网关服务功能
@@ -16,10 +18,24 @@ public class ApiGatewayDynamicRouteApplication {
         SpringApplication.run(ApiGatewayDynamicRouteApplication.class, args);
     }
 
+    /**
+     * 启动过滤器
+     * @return
+     */
     @Bean
-    @RefreshScope       //将Zuul的配置内容动态化
-    @ConfigurationProperties("zuul")
-    public ZuulProperties zuulProperties() {
-        return new ZuulProperties();
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
+
+    /**
+     * 创建类似/v1/userservice/**的路由匹配规则
+     * @return
+     */
+    @Bean
+    public PatternServiceRouteMapper serviceRouteMapper() {
+        return new PatternServiceRouteMapper(
+                "(?<name>^.+)-(?<version>v.+$)",      //匹配服务名称是否复合该自定义规则的正则表达式
+                "${version}/${name}"      //定义根据服务名中定义的内容转换出的路径表达式规则
+        );
     }
 }
---
+++ api-gateway-dynamic-route/src/main/java/com/yeta/apigatewaydynamicroute/filter/AccessFilter.java
@@ -0,0 +1,77 @@
+package com.yeta.apigatewaydynamicroute.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 在请求被路由之前检查HttpServletRequest中是否有accessToken参数，若有就进行路由，若没有就拒绝访问，返回401 Unauthorized错误
+ * @author YETA
+ * @date 2019/03/15/17:54
+ */
+public class AccessFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(ZuulFilter.class);
+
+    /**
+     * 过滤器的类型
+     * pre表示在请求被路由之前执行
+     * routing表示在路由请求时被调用
+     * post表示在routing和error过滤器之后被调用
+     * error表示处理请求时发生错误时被调用
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    /**
+     * 过滤器的执行顺序
+     * 数字越小优先级越高
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 判断该过滤器是否需要被执行
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 过滤器的具体逻辑
+     * @return
+     * @throws ZuulException
+     */
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+
+        log.info("send {} request to {}", request.getMethod(), request.getRequestURL().toString());
+
+        Object accessToken = request.getParameter("accessToken");
+        if (accessToken == null) {
+            log.warn("access token is empty");
+            requestContext.setSendZuulResponse(false);      //不进行路由
+            requestContext.setResponseStatusCode(401);      //设置返回错误码
+            //requestContext.setResponseBody("access token is empty");        //设置返回内容
+            
+            return null;
+        }
+
+        log.info("access token is ok");
+        return null;
+    }
+}
